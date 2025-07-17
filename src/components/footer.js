d75a245b1925c793261efe039d847ac8f25187a9
KevinRyans <michaelwulsch@gmail.com>
2025-07-17 22:36:31 +0200

    Delete src directory

--- src/components/footer.js
+++
@@ -1,6 +0,0 @@
-export function createFooter() {
-  const footer = document.createElement('footer');
-  footer.className = 'text-center py-8 text-sm text-gray-400';
-  footer.innerHTML = `&copy; ${new Date().getFullYear()} Ola Utvikler`;
-  return footer;
-}
